b561dd9fb784444399c2301d67b113a3cae01ddf
Artem <artem09022004@gmail.com>
2023-05-08 13:48:24 +0700

    Updating the bot and file contents .gitignore

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
+import os
 import random
 
 import discord
 from discord.ext import commands
-
-import config
-
-discord_intents = discord.Intents.all()
-discord_intents.members = True
-
-bot = commands.Bot(command_prefix='>', intents=discord_intents)
-bot.remove_command('help')
+from dotenv import load_dotenv
 
 
-@bot.event
-async def on_ready():
-    print(bot.user.name, 'Работает!')
+class DiceBot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.remove_command('help')
 
+        @self.event
+        async def on_ready():
+            print(f'{self.user.name} has connected to Discord!')
 
-@bot.listen()
-async def on_message(message: discord.message.Message):
-    try:
-        if message.author != bot.user and message.content[0] == '!':
-            dices = message.content[1:].split('+')
-            nums = []
-            for dice in dices:
-                if len(ns := tuple(dice.split('d'))) == 2:
-                    count, num = ns
-                    for _ in range(int(count)):
-                        nums.append(str(random.randint(1, int(num))))
-                else:
-                    nums.extend(ns)
-            content = f'```md\n#{sum(list(map(int, nums)))}\nDetails:[{message.content[1:]} ({" ".join(nums)})]```'
-            await message.reply(content=content, mention_author=False)
-    except ValueError:
-        print('Неизвестная команда')
+        @self.listen()
+        async def on_message(message):
+            if message.author == self.user or not message.content.startswith('!'):
+                return
+            try:
+                nums = []
+                for dice in message.content[1:].split('+'):
+                    count, num = dice.split('d') if 'd' in dice else (1, dice)
+                    nums.extend(str(random.randint(1, int(num))) for _ in range(int(count)))
+                total = sum(map(int, nums))
+                content = f'```md\n#{total}\nDetails:[{message.content[1:]} ({" ".join(nums)})]```'
+                await message.reply(content=content, mention_author=False)
+            except ValueError:
+                print('Неизвестная команда')
 
 
 if __name__ == '__main__':
-    bot.run(config.token)
+    load_dotenv()
+    bot = DiceBot(command_prefix='>', intents=discord.Intents().all())
+    bot.run(os.getenv('DISCORD_BOT_TOKEN'))
